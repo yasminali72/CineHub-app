2c7bba3591cb58027136d8b722ff5878ee3feb71
yasminali72 <yasminali7374@gmail.com>
2024-09-19 20:42:31 +0300

    Done

--- src/Commponents/BannerHome/BannerHome.jsx
+++ src/Commponents/BannerHome/BannerHome.jsx
@@ -25,7 +25,7 @@ console.log(bannerData,'bannnnn');
   return (
    <>
    
-   <section className="w-full h-full overflow-x-hidden">
+   <section className="w-full h-full ">
       <Slider {...settings}>
         {bannerData.map((data) => (
           
