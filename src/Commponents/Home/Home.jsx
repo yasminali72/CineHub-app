d364d03d4bedc4d4d1a0d96fd9438a1bde543f6b
yasminali72 <yasminali7374@gmail.com>
2024-09-20 02:41:38 +0300

    Done

--- src/Commponents/Home/Home.jsx
+++ src/Commponents/Home/Home.jsx
@@ -8,7 +8,8 @@ import Card from "../Card/Card";
 import Slider from "react-slick";
 
 export default function Home() {
-  const { bannerData, imageURL } = useSelector((state) => state.movieoData);
+  const dispatch = useDispatch();
+  const { bannerData} = useSelector((state) => state.movieoData);
   const [nowPlayingData, setNowPlayingData] = useState([]);
   const [topRated, setTopRated] = useState([]);
   const [popular, setPopular] = useState([]);
@@ -20,7 +21,38 @@ export default function Home() {
   const [isDisabled, setIsDisabled] = useState(false);
   const [notFound, setNotFound] = useState(false);
 
- 
+  const getTeanding = async () => {
+    try {
+      let { data } = await axios.get(
+        "http://api.themoviedb.org/3/trending/all/week",
+        {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGNjMjM4ZmE0MjIwMTliZjdlNmQyNzBiNjZmNjY1YyIsIm5iZiI6MTcyNjMzNTc1Mi42NTY3NTYsInN1YiI6IjY2ZTViYjM4ZWEyOTY5ODY0ZDc0YmZlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.44BHSGVL4cN29tW3ehGHeIzC48j6olcGIhhkMAgLDQI`,
+          },
+        }
+      );
+      dispatch(setBannerData(data.results));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getCconfiguration = async () => {
+    try {
+      let { data } = await axios.get(
+        "https://api.themoviedb.org/3/configuration",
+        {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGNjMjM4ZmE0MjIwMTliZjdlNmQyNzBiNjZmNjY1YyIsIm5iZiI6MTcyNjMzNTc1Mi42NTY3NTYsInN1YiI6IjY2ZTViYjM4ZWEyOTY5ODY0ZDc0YmZlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.44BHSGVL4cN29tW3ehGHeIzC48j6olcGIhhkMAgLDQI`,
+          },
+        }
+      );
+      console.log("dat", data);
+      dispatch(setImageURL(data.images.secure_base_url + "original"));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const getNowPlayingData = async () => {
     if (numOfPage === 1) {
@@ -140,7 +172,8 @@ export default function Home() {
   };
 
   useEffect(() => {
-   
+    getTeanding();
+    getCconfiguration();
     getNowPlayingData();
     getTopRated();
     getPopular();
