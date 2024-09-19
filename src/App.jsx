8120e445066249e467a1cbc28d6f9a4b63135501
yasminali72 <yasminali7374@gmail.com>
2024-09-19 21:39:03 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,46 +4,11 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import { RouterProvider } from 'react-router-dom';
 
+import { router } from './routers';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import Login from './Commponents/Login/Login';
-import Search from './Commponents/Search/Search';
-import Details from './Commponents/Details/Details';
-import ExpolerPage from './Commponents/ExpolerPage/ExpolerPage';
-import Home from './Commponents/Home/Home';
-import Layout from './Commponents/Layout/Layout';
 function App() {
 
-  const router=createBrowserRouter([
-    {
-      path:'',
-      element:<Layout/>,
-      children:[
-        {index:true,
-      element:<Home/>
-    
-    },
-    {
-      path:':expoler',
-      element:<ExpolerPage/>
-    }
-    ,{
-      path:':expoler/:id',
-      element:<Details/>
-    },{
-      path:'search'
-      ,element:<Search/>
-    }
-  
-      ,{
-      path:'login',
-      element:<Login/>
-    }
-   
-
-      ]
-    }
-  ])
 
   return (
     <>
